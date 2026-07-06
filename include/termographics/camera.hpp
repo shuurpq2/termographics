de19bdc8aefd8e49ca632a8853ed9047b705541c
savethebees <afghanistaneurobolivar@gmail.com>
2026-07-06 16:21:22 +0300

    add setters and getters

--- include/termographics/camera.hpp
+++ include/termographics/camera.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "termographics/vector3i.hpp"
+#include <memory>
 
 namespace termg
 {
@@ -15,7 +16,14 @@ public:
     Camera(Vector3I pos = Vector3I(0, 0, 0), int rotation_angle = 0) : m_pos(pos), m_rot_angle(rotation_angle) {}
 
     Vector3I get_pos() { return m_pos; }
+
+    void set_pos(Vector3I new_pos) { m_pos = std::move(new_pos); }
+
     int get_rotation_angle() { return m_rot_angle; }
+
+    void set_rotation_angle(int new_angle) { m_rot_angle = new_angle; }
+
+    void add_rotation_angle(int delta) { m_rot_angle += delta; }
 };
 
 }
