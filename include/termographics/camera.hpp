fa673873d813e156efd9c59f8cda908eba63ee1e
savethebees <shurup1337shurup@gmail.com>
2026-07-24 11:32:44 +0300

    camera refactor + camera_test update

--- include/termographics/camera.hpp
+++ include/termographics/camera.hpp
@@ -2,7 +2,8 @@
 
 #include "termographics/vector3i.hpp"
 #include <memory>
-#include <math.h>
+#include <cmath>
+#include <algorithm>
 
 namespace termg
 {
@@ -10,23 +11,44 @@ namespace termg
 class Camera
 {
 private:
-    Vector3I m_pos = Vector3I(0, 0, 0);
-    int m_yaw = 0;
+    struct Rotation
+    {
+        float yaw;
+        float pitch;
+    };
 
-    inline int normalize_yaw(int angle) { return (angle % 360 + 360) % 360; }
-    // TODO: pitch
 public:
-    Camera(Vector3I pos = Vector3I(0, 0, 0), int yaw = 0) : m_pos(pos), m_yaw(normalize_yaw(yaw)) {}
+    static constexpr float MAX_YAW = 180.0f;
+    static constexpr float MIN_YAW = -180.0f;
+    static constexpr float MAX_PITCH = 90.0f;
+    static constexpr float MIN_PITCH = -90.0f;
+
+    Camera(Vector3I pos = Vector3I(0, 0, 0), float yaw = 0, float pitch = 0) : m_pos(pos), m_rotation({normalize_yaw(yaw), normalize_pitch(pitch)}) {}
 
     Vector3I get_pos() { return m_pos; }
-
     void set_pos(Vector3I new_pos) { m_pos = std::move(new_pos); }
 
-    int get_yaw() { return m_yaw; }
+    Rotation get_rotation() { return m_rotation; }
+    void set_yaw(int angle) { m_rotation.yaw = normalize_yaw(angle); }
+    void set_pitch(int angle) { m_rotation.pitch = normalize_pitch(angle); }
+    void set_rotation(int yaw, int pitch) { set_yaw(yaw); set_pitch(pitch); }
+    void add_yaw(int delta) { m_rotation.yaw = normalize_yaw(m_rotation.yaw + delta); }
+    void add_pitch(int delta) { m_rotation.pitch = normalize_pitch(m_rotation.pitch + delta); }
 
-    void set_yaw(int angle) { m_yaw = normalize_yaw(angle); }
+private:
+    Vector3I m_pos = Vector3I(0, 0, 0);
+    Rotation m_rotation = {.yaw=0.0f, .pitch=0.0f};
 
-    void add_yaw(int delta) { m_yaw = normalize_yaw(m_yaw + delta); }
+    inline float normalize_yaw(float angle)
+    {
+        angle = std::fmod(angle, 360.0f);
+
+        if (angle >= MAX_YAW) angle -= 360.0f;
+        else if (angle < MIN_YAW) angle += 360.0f;
+
+        return angle;
+    }
+    inline float normalize_pitch(float angle) { return std::min(std::max(angle, MIN_PITCH), MAX_PITCH); }
 };
 
 }
--- test/camera_test.cpp
+++ test/camera_test.cpp
@@ -4,11 +4,17 @@
 TEST(CameraTestSuite, Constructor_IntendedUse_SavesDataCorrectly)
 {
     termg::Vector3I camera_pos(30, 12, 51);
-    int camera_yaw = 543534;
-    termg::Camera camera(camera_pos, camera_yaw);
+    float camera_yaw = 543534.61f;
+    float camera_pitch = 881080.23f;
+    termg::Camera camera(camera_pos, camera_yaw, camera_pitch);
 
     EXPECT_EQ(camera.get_pos().x, camera_pos.x);
     EXPECT_EQ(camera.get_pos().y, camera_pos.y);
     EXPECT_EQ(camera.get_pos().z, camera_pos.z);
-    EXPECT_TRUE(camera.get_yaw() >= 0 && camera.get_yaw() < 360);
+    
+    float current_yaw = camera.get_rotation().yaw;
+    float current_pitch = camera.get_rotation().pitch;
+
+    EXPECT_TRUE(termg::Camera::MAX_YAW >= current_yaw && current_yaw >= termg::Camera::MIN_YAW);
+    EXPECT_TRUE(termg::Camera::MAX_PITCH >= current_pitch && current_pitch >= termg::Camera::MIN_PITCH);
 }
